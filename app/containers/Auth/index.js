228df576bc879c4035b43f69cb9c9a7a28640c54
Alfonso M. García Astorga <me@alfon.io>
2018-09-24 02:52:16 +0200

    update

--- app/containers/Auth/index.js
+++ app/containers/Auth/index.js
@@ -33,28 +33,6 @@ export function initAuthenticationMechanism(store) {
       return 'INVALID_TOKEN';
     }
   }
-  function scheduleAccessTokenRenewal() {
-    const getDiffTimeInSeconds =
-      JWTDecode(accessToken).exp - +new Date() / 1000;
-
-    if (getDiffTimeInSeconds <= 600) {
-      return api.refreshAccessToken(refreshToken).then(newAccessToken => {
-        accessToken = newAccessToken;
-        store.dispatch(setAccessToken(accessToken));
-
-        scheduleAccessTokenRenewal();
-      });
-    }
-
-    return setTimeout(() => {
-      api.refreshAccessToken(refreshToken).then(newAccessToken => {
-        accessToken = newAccessToken;
-        store.dispatch(setAccessToken(accessToken));
-
-        scheduleAccessTokenRenewal();
-      });
-    }, 500000);
-  }
 
   try {
     accessToken = localStorage.getItem('access_token');
@@ -65,16 +43,8 @@ export function initAuthenticationMechanism(store) {
     const accessTokenStatus = validateJSONWebToken(accessToken);
     const refreshTokenStatus = validateJSONWebToken(refreshToken);
 
-    if (accessTokenStatus === 'OK' && refreshTokenStatus === 'OK') {
+    if (accessTokenStatus !== 'INVALID_TOKEN' && refreshTokenStatus === 'OK')
       logInUser();
-      scheduleAccessTokenRenewal();
-    } else if (accessTokenStatus === 'EXPIRED' && refreshTokenStatus === 'OK') {
-      api.refreshAccessToken(refreshToken).then(newAccessToken => {
-        accessToken = newAccessToken;
-        logInUser();
-        scheduleAccessTokenRenewal();
-      });
-    }
   } catch (e) {
     throw e;
   }
