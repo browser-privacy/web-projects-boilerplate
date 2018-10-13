88ab660330363089dab68578605887a0e807c349
Alfonso M. García Astorga <me@alfon.io>
2018-10-13 21:22:49 +0200

    update

--- app/containers/UserDashboard/EmailVerificationPage/tests/reducer.test.js
+++
@@ -1,8 +0,0 @@
-import { fromJS } from 'immutable';
-import emailVerificationPageReducer from '../reducer';
-
-describe('emailVerificationPageReducer', () => {
-  it('returns the initial state', () => {
-    expect(emailVerificationPageReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
--- app/containers/UserDashboard/EmailVerificationPage/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectEmailVerificationPageDomain } from '../selectors';
-
-describe('selectEmailVerificationPageDomain', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
-  });
-});
