8322cd70cc1b0612e1724711f835566326cef75b
Alfonso M. García Astorga <me@alfon.io>
2018-10-21 08:14:22 +0200

    Add localStorage API

---
+++ app/api/vendors/localStorage.js
@@ -0,0 +1,25 @@
+const { localStorage } = window;
+
+export function setItem(key, value) {
+  try {
+    localStorage.setItem(key, JSON.stringify(value));
+  } catch (_) {
+    console.error(`Error writing to localStorage`, key, value);
+  }
+}
+
+export function getItem(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (err) {
+    return undefined;
+  }
+}
+
+export function clear() {
+  try {
+    localStorage.clear();
+  } catch (_) {
+    console.error('Error clearing localStorage');
+  }
+}
