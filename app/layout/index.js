918e41de29e6f86663eb072413e713497f3b4285
Alfonso M. García Astorga <me@alfon.io>
2018-10-22 09:50:28 +0200

    add layout

---
+++ app/layout/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const DefaultLayout = ({ children }) => (
+  <React.Fragment>
+    <Navbar />
+    {children}
+    <Footer />
+  </React.Fragment>
+);
+
+DefaultLayout.propTypes = {
+  children: PropTypes.object,
+};
+
+export default DefaultLayout;
