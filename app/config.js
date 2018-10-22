51f668f8c616fe963d2dc4dc311014bd46a6e6b7
Alfonso M. García Astorga <me@alfon.io>
2018-10-22 12:10:31 +0200

    add recaptcha site key

--- app/config.js
+++ app/config.js
@@ -1,12 +1,15 @@
 export const config = {
   test: {
     API_ENDPOINT: 'http://localhost:3000',
+    RECAPTCHA_SITE_KEY: '',
   },
   development: {
     API_ENDPOINT: 'http://localhost:3000',
+    RECAPTCHA_SITE_KEY: '',
   },
   production: {
     API_ENDPOINT:
       'https://x1pct1mfd7c.execute-api.us-east-1.amazonaws.com/prod',
+    RECAPTCHA_SITE_KEY: '',
   },
 };
